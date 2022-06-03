597f89c17394fc68768af15c60ec4c8b208166ef
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-06-03 20:23:52 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -118,6 +118,7 @@ async def start(bot, message):
                 InlineKeyboardButton("Search Here", switch_inline_query_current_chat='')
                 ],[
                 InlineKeyboardButton("Help", callback_data="help"),
+                InlineKeyboardButton("Discussion Group', url='https://telegram.me/TamilMV_Collections'),
                 InlineKeyboardButton("About", callback_data="about")
                 ]]
             )
